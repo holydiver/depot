57f54b175bb1eb717b3b4f75160aa5a0aadd6794
Enrique Temes <enrtemes@gmail.com>
2010-08-19 22:04:01 -0400

    Line item price

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -38,7 +38,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "image_url" do
-    ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
             http://a.b.c/x/y/z/fred.gif }
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
 
