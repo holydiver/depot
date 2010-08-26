9490ac4f043e58a815c698feb8e20f67d09ba1a3
Enrique Temes <enrtemes@gmail.com>
2010-08-25 22:51:12 -0400

    ajax + orders

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,8 +47,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart) } 
-
+        format.html { redirect_to(store_url) } 
+        format.js   { @current_item = @line_item }
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
@@ -81,7 +81,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to(cart) }
+      format.html { redirect_to(store_path) }
       format.xml  { head :ok }
     end
   end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @cart = current_cart
     @current_time = Time.now.strftime("%m-%d-%Y %H:%M:%S")
   end
 end
