d004e6e7dfeab4f7ffb0349bcb96920b9b2d226e
Enrique Temes <enrtemes@gmail.com>
2010-09-10 07:20:16 -0400

    added counter to session from storecontroller#index

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,5 +5,13 @@ class StoreController < ApplicationController
     @products = Product.all
     @cart = current_cart
     @current_time = Time.now.strftime("%m-%d-%Y %H:%M:%S")
+
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+
+    @counter = session[:counter]
   end
 end
